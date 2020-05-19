fa6344db97a1c192f20a3dda18aaf77957158399
Nils Kuhn <nils.kuhn@iteratec.de>
2020-05-19 13:30:08 +0200

    :bug: Configure explorer correctly on startup

--- explorer/runexplorer.sh
+++ explorer/runexplorer.sh
@@ -3,19 +3,21 @@
 echo "Sleep for 30 seconds so the master node has initialised"
 sleep 30
 
-echo "Start the chain"
-multichaind -daemon -txindex -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE:$NETWORK_PORT
-
-echo "Sleep for 30 seconds so the slave node has initialised"
-sleep 30
-
-echo "Setup /root/.multichain/$CHAINNAME/multichain.conf"
-cat << EOF > /root/.multichain/$CHAINNAME/multichain.conf
+echo "Setup /root/.multichain/multichain.conf"
+mkdir -p /root/.multichain/
+cat << EOF > /root/.multichain/multichain.conf
 rpcuser=$RPC_USER
 rpcpassword=$RPC_PASSWORD
 rpcallowip=$RPC_ALLOW_IP
 rpcport=$RPC_PORT
 EOF
+cp /root/.multichain/multichain.conf /root/.multichain/$CHAINNAME/multichain.conf
+
+echo "Start the chain"
+multichaind -daemon -txindex -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE:$NETWORK_PORT
+
+echo "Sleep for 30 seconds so the slave node has initialised"
+sleep 30
 
 echo "Setup /root/explorer.conf"
 cat << EOF > /root/explorer.conf
